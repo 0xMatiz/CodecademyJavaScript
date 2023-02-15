1d206531692a02957a2a0c190fe569edb15a9f10
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-15 04:24:26 -0300

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -79,3 +79,30 @@ Then log pastaIndex to the console. (You may remove the other console.log() stat
 const pastaIndex = groceryList.indexOf('pasta');
 console.log(pastaIndex);
 
+// LESSON 10 - Arrays and Functions
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr) {
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+
+/* 1. In main.js, there is an array concept. There is also a function changeArr that will assign the element in index 3 of an array to 'MUTATED'. The function changeArr was called with an argument of concept.
+Underneath the function call, log concept to the console to check if this reassignment mutated the array. */
+console.log(concept);
+
+/* 2. Let’s double check what happens if we mutate an array using a built-in method inside a function.
+Under the console.log() statement, define another function named removeElement that takes a parameter of newArr. Inside the function body call .pop() on newArr. */
+function removeElement(newArr) {
+  newArr.pop();
+} 
+
+//3. Call removeElement() with an argument of concept.
+removeElement(concept);
+
+/* 4. 
+After calling removeElement(concept), check the value of concept by logging it to console.
+Notice that in both cases, the change to the array was maintained outside of the function! */
+console.log(concept);
+
