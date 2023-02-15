943c8c9e35450f20602b35ba61ebbf2ec4d26270
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-15 04:09:36 -0300

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,4 +1,4 @@
-//LESSON 5
+//LESSON 5 - Arrays with let and const
 let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
 
 const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
@@ -22,3 +22,27 @@ Log the updated array to the console. */
 utensils[3] = 'Spoon';
 console.log(utensils);
 
+//LESSON 6 - The .length property
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+
+//1. Find the length of the objectives array and log it to the console.
+console.log(objectives.length);
+
+//LESSON 7 - The .push() Method
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+// 1. Add two elements to the chores array using .push().
+chores.push('organize the house', 'clean the table');
+console.log(chores);
+
+//LESSON 8 - The .pop() Method
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+//1. Use the .pop() method to remove the last element from chores.
+const removed = chores.pop();
+
+/* 2. In a line after you called chores.pop(), log chores to the console to make sure it worked. */
+console.log(chores);
+console.log(removed);
+
+//LESSON 9
