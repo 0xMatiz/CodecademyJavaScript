e6c44bd0304c2f0f3ab8bc64aa468083726e59f9
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-15 04:13:39 -0300

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -45,4 +45,37 @@ const removed = chores.pop();
 console.log(chores);
 console.log(removed);
 
-//LESSON 9
+//LESSON 9 - More Array Methods
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+/* 1. Use the .shift() method to remove the first item from the array 
+groceryList. */
+groceryList.shift(0);
+//console.log(groceryList); //Im comenting this instead of erasing it.
+
+/* 2. Under the code added in step 1, use the .unshift() method to add 'popcorn' to the beginning of your grocery list.
+After calling .unshift() on groceryList, log groceryList to the console.
+You may wish to delete the console.log() statement from the previous 
+step. */
+groceryList.unshift('popcorn');
+console.log(groceryList); //Im comenting this instead of erasing it.
+
+/* 3. You’re in a hurry so you decide to ask a friend to help you with your grocery shopping. You want him to pick up the 'bananas', 'coffee beans', and 'brown rice'.
+Under the code you added for step 2, use .slice() to provide your friend with a list of these three things.
+Log this part of the list to the console. Unlike the two previous checkpoints, you should do both of these steps in one line. */
+console.log(groceryList.slice(1, 4)); 
+// I've put 1 for 'bananas' and put 4 for 'pasta'
+
+/* 4. After calling .slice() on groceryList, log the grocery list to the console one more time.
+Notice that the groceryList array still contains the same items it had in Step 2. That means .slice() is non-mutating! You can confirm this in the link in the previous step. */
+console.log(groceryList);
+
+/* 5. Let’s find the index of a particular element in groceryList using .indexOf().
+
+Call .indexOf() on groceryList to find the index of the element 'pasta' and save the returned value to a const variable named pastaIndex.
+
+Then log pastaIndex to the console. (You may remove the other console.log() statements to declutter the terminal.) */
+const pastaIndex = groceryList.indexOf('pasta');
+console.log(pastaIndex);
+
