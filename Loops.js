4b7fb2187c03dfd5611004415580b74258a01eb8
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-16 00:09:50 -0300

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,16 @@
+/* - LOOPS -
+ LESSON 1 - Repeating Tasks Manually
+
+1.Create the variable vacationSpots, and assign its value to an array of three strings naming places you’d like to visit.
+Write your code below */
+let vacationSpots = ['Japan','New York', 'Germany'];
+
+/* 2. Next, console.log() each item in vacationSpots. Since we don’t know loops yet, we have to console.log() each element in the array separately. */
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+/* 3. Nice work! Now imagine that the vacation list had 100 places on it— logging each array element to the console by hand would be a tedious task! In the next exercise, we will learn how to make things more efficient with for loops. */
+
+//Okay i imagine 😂
+
