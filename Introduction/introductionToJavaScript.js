56b3cb3aea7e137c9a2e9147c1afd62031515d82
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-16 04:34:36 -0300

    UPDATE

---
+++ Introduction/introductionToJavaScript.js
@@ -0,0 +1,60 @@
+//LESSON 2 - Console
+
+/* 1. Use the console.log code in the editor to log your age to the console.
+Run your code when you are ready to see the result.
+
+/* 2. On the next line, write another console.log to print out a different number representing the number of weeks you’ve been programming. */
+
+console.log(5);
+console.log(2);
+
+
+//LESSON 3 - Comments
+
+/* 1. Let’s practice adding some code comments.
+To the right, we’ve provided you with the beginning of the book Catch-22 by Joseph Heller.
+On line 1, write a single line comment that says Opening line. */
+
+/* 2. Single line comments are great for adding context to your code. Multi-line comments are often best suited to prevent a block of code from running. However, both types of comments can be used for either purpose.
+Use a multi-line comment so that the bottom 6 console.log() statements are all commented out. */
+
+// Opening line
+
+console.log('It was love at first sight.');
+
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it.');
+/* wasn\'t quite jaundice.*/
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+
+
+//LESSON 4 - Data Types
+
+/* 1. On line 1, log the string 'JavaScript' to the console.
+   2. On line 2, log the number 2011 to the console.
+   3. On line 3, print 'Woohoo! I love to code! #codecademy' to the console.
+   4.On line 4, print the number 20.49 to the console. */
+
+console.log("JavaScript");
+console.log(2011);
+console.log("Woohoo! I love to code! #codecademy");
+console.log(20.49)
+
+
+//LESSON 5 - Arithmetic Operators
+
+/* 1. Inside of a console.log(), add 3.5 to your age. This is the age you’ll be when we start sending people to live on Mars.
+   2. On a new line write another console.log(). Inside the parentheses, take the current year and subtract 1969. The answer is how many years it’s been since the 1969 moon landing.
+   3. Create another console.log(). Inside the parentheses divide 65 by 240.
+   4. Create one last console.log(). Inside the parentheses, multiply 0.2708 by 100. That’s the percent of the sun that is made up of helium. Assuming we could stand on the sun, we’d all sound like chipmunks! */
+
+console.log(3.5 + 24);
+console.log(2023 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
+
+
+//LESS
