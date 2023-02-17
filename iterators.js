e62970aa2e4ff31f5bde15c1e547819ddb9a16b6
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-17 15:58:41 -0300

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -102,3 +102,33 @@ return animal[0](search the first character of each animal that is why 'animal' 
 ? true : false (checks if this is true, if it is returns a value, if not, doesn't returns anything)
 
 /* "animals" is different from 'animal' in the function because it is a callback function */
+
+
+//ITERATORS - LESSON 6: The .reduce() Method
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10); //This adds an initial value of 10 to accumulator
+
+console.log(newSum); 
+
+/* 1. Let’s practice calling .reduce() and using console.log() to check the values as .reduce() iterates through the array.
+In main.js, there is an array of numbers, newNumbers.
+To start, declare a new variable named newSum using the const keyword. Assign to newSum the value of calling .reduce() on newNumbers. You don’t need to provide any arguments to .reduce() yet.
+You’ll also see a TypeError: undefined is not a function but we’ll fix that after we add our callback function in the next step! */
+
+/* 2. Provide .reduce with an argument of a callback function. The callback function has two parameters. The first parameter is accumulator and the second parameter is currentValue. Use either a function expression or an arrow function. */
+
+/* 3. To check the value being used as we iterate through the array, add three statements to the function body of the callback:
+console.log('The value of accumulator: ', accumulator);
+console.log('The value of currentValue: ', currentValue);
+a return statement that adds accumulator to currentValue. */
+
+/* 4. Log the value of newSum to the console to see the return value of calling .reduce() on newNumbers. */
+
+/* 5. While we have this code set up, let’s also check what happens if you add a second argument to .reduce(). The second argument acts as an initial value for the accumulator.
+Add a second argument of 10 to .reduce(). */
