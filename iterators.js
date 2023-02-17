62476c80afb87f798fdceafa98b1c2af2e191e69
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-17 12:34:44 -0300

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -1,3 +1,14 @@
 //MODULE: ITERATORS
 
-//LESSON 2
+//LESSON 2 - The .forEach() Method
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+
+/* 1. Iterate over the fruits array to log I want to eat a plus the name of each fruit to the console. For example, I want to eat a mango.
+You may use any form of callback you prefer. */
+
+fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`));
+
+
