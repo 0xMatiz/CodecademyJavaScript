4b4c0a7e63dad49fbb06b0fe128854487341a379
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-17 14:37:19 -0300

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -12,3 +12,49 @@ You may use any form of callback you prefer. */
 fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`));
 
 
+//LESSON 3 - The .map() Method
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animals => {
+  return animals[0];
+})
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(bignumbers => {
+  return bignumbers / 100;
+  console.log(smallNUmbers);//LESSON 3 - The .map() Method
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animals => {
+  return animals[0];
+})
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(bigNumbers => {
+  return bigNumbers / 100;
+})
+
+
+/* 1. Add your code under the animals array and before the line console.log(secretMessage.join(''));
+Use .map() to create a new array that contains the first character of each string in the animals array. Save the new array to a const variable named secretMessage. */
+
+/* 2. Use .map() to divide all the numbers in bigNumbers by 100. Save the returned values to a variable declared with const called smallNumbers. */
+
+})
+
+/* 1. Add your code under the animals array and before the line console.log(secretMessage.join(''));
+Use .map() to create a new array that contains the first character of each string in the animals array. Save the new array to a const variable named secretMessage. */
+
+/* 2. Use .map() to divide all the numbers in bigNumbers by 100. Save the returned values to a variable declared with const called smallNumbers. */
