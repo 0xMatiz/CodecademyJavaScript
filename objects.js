3e1285f38f07bc872bc384ccbf8f608d7bd9559d
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-17 21:22:50 -0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -23,3 +23,23 @@ let fasterShip = {
     /* 1. The spaceship we have so far looks good but, unfortunately, is not very fast at hyperspace travel due to having an inferior fuel source. Make a new spaceship object named fasterShip with the same color as spaceship but with a Fuel Type equal to 'Turbo Fuel'. */
 
 
+//LESSON 3 - Accessing Properties
+
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+  };
+  
+  // Write your code below
+  
+  let crewCount = spaceship.numCrew;
+  
+  let planetArray = spaceship.flightPath;
+  
+  /* 1. Let’s use the dot operator to access the value of numCrew from the spaceship object in the code editor. Create a variable crewCount and assign the spaceship‘s numCrew property to it.
+  
+  2. Again using the dot operator, create a variable planetArray and assign the spaceship‘s flightPath property to it. */
+  
