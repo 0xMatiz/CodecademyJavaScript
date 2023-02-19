edcf36fa523087171a506488b8121606c99bb33a
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-19 01:51:44 -0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -157,3 +157,33 @@ When you want to target an index in an array just put one addition [] per each a
 */
 
 
+//LESSON 8 - Pass By Reference
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+
+let greenEnergy = spaceship => {
+ spaceship['Fuel Type'] = 'avocado oil';
+};
+
+let remotelyDisable = obj => {
+  obj.disabled = true;
+};
+/* Whenever you declare a function, "spaceship" or "obj" could have any name you want, you are going to change it later anyways. 
+Now when we put real object name like in the function call below, that value is the one who the function is going to target*/
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+
+console.log(spaceship);
+
+/*
+1. Write a function greenEnergy() that has an object as a parameter and sets that object’s 'Fuel Type' property to 'avocado oil'.
+2. Write a function remotelyDisable() that has an object as a parameter and sets (or reassigns) that object’s disabled property to true.
+3. Call your two functions with the spaceship object in the code editor, then console.log() the spaceship object to confirm those properties were changed/added.
+*/
+
+
