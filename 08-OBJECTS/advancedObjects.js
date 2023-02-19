8c10bba006dfb11bed0a9dcaf38dffbb5deabade
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-19 18:09:20 -0300

    Update advancedObjects.js

--- 08-OBJECTS/advancedObjects.js
+++ 08-OBJECTS/advancedObjects.js
@@ -21,3 +21,27 @@ Replace ‘MODEL’ and ‘ENERGYLEVEL’ with the calling object’s model and 
 */
 
 
+//LESSON 3 - Arrow Functions and this
+
+const robot = {
+  energyLevel: 100,
+  checkEnergy() {
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
+
+robot.checkEnergy();
+
+
+
+/*1.Currently the .checkEnergy() method is not producing the correct output because it is using arrow function syntax.
+
+Refactor, or change, the method to use a function expression. You can write the function using either longhand or shorthand format.
+
+After refactoring the method, notice that .checkEnergy() has access to the other internal properties of the robot object.
+
+Arrow Functions and "this." method doesnt get along. Because the arrow function makes the "this." method point to its target on a global scope.
+This by consecuence returns undefined unless there is a global scope target equal to ours.
+*/
+
+
