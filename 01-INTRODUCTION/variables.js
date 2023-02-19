f85e708ec797d239854bb981c1934e083ffa0d86
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-19 06:49:38 -0300

    update

--- 01-INTRODUCTION/variables.js
+++ 01-INTRODUCTION/variables.js
@@ -28,3 +28,108 @@ To check if changeMe was reassigned, log the value saved to changeMe to the cons
 */
 
 
+//LESSON 4 - Create a Variable: const
+
+const entree = 'Enchiladas'
+console.log(entree);
+entree = 'Tacos';
+
+/*
+1. Create a constant variable named entree and set it to equal to the string 'Enchiladas'.
+2. Just to check that you’ve saved the value of 'Enchiladas' to entree, log the value of entree to the console.
+3. Great, let’s see what happens if you try to reassign a constant variable.
+
+Paste the following code to the bottom of your program.
+
+entree = 'Tacos'
+This code throws the following error when you run your code:
+
+TypeError: Assignment to constant variable.
+After you clear this checkpoint, if you want to see about another quirk of const in action open the hint!
+*/
+
+
+//LESSON 5 - Mathematical Assignment Operators
+
+let levelUp = 10;
+levelUp += 5;
+let powerLevel = 9001;
+powerLevel -= 100;
+let multiplyMe = 32;
+multiplyMe *= 11;
+let quarterMe = 1152;
+quarterMe /= 4;
+
+// Use the mathematical assignments in the space below:
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+/*
+1. Use the += mathematical assignment operator to increase the value stored in levelUp by 5.
+2.Use the -= mathematical assignment operator to decrease the value stored in powerLevel by 100.
+3. Use the *= mathematical assignment operator to multiply the value stored in multiplyMe by 11.
+4. Use the /= mathematical assignment operator to divide the value stored in quarterMe by 4.
+*/
+
+
+//LESSON 6 - The Increment and Decrement Operator
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+lostDollar--;
+
+/*
+1. Using the increment operator, increase the value of gainedDollar.
+2. Using the decrement operator, decrease the value of lostDollar.
+*/
+
+
+//LESSON 7 - String Concatenation with Variables
+
+let favoriteAnimal = 'dog';
+console.log('My favorite animal: ' + favoriteAnimal);
+
+/*
+1. Create a variable named favoriteAnimal and set it equal to your favorite animal.
+2. Use console.log() to print 'My favorite animal: ANIMAL' to the console. Use string concatenation so that ANIMAL is replaced with the value in your favoriteAnimal variable.
+*/
+
+
+//LESSON 8 - String Interpolation
+
+let myName = 'Matias';
+let myCity = 'Buenos Aires';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
+/*
+1. Create a variable called myName and assign it your name.
+2. Create a variable called myCity and assign it your favorite city’s name.
+3. Use a single template literal to interpolate your variables into the sentence below. Use console.log() to print your sentence to the console in the following format:
+
+My name is NAME. My favorite city is CITY.
+Replace NAME and CITY in the string above by interpolating the values saved to myName and myCity.
+*/
+
+
+//LESSON 9 - typeof operator
+
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
+
+/*
+1. Use console.log() to print the typeof newVariable.
+2. Great, now let’s check what happens if we reassign the variable. Below the console.log() statement, reassign newVariable to 1.
+3. Since you assigned this new value to newVariable, it has a new type! On the line below your reassignment, use console.log() to print typeof newVariable again.
+*/
+
+
+//VARIABLES LESSON FINISHED!
--- 05-ARRAYS/arrays.js
+++ 05-ARRAYS/arrays.js
@@ -1,3 +1,51 @@
+//LESSON 2 - Create an Array
+
+const hobbies = ['1','2','3'];
+console.log(hobbies);
+
+/*
+1. Declare a variable using const named hobbies and set it equal to an array with three strings inside of it.
+2. Use console.log() to print hobbies to the console. 
+*/
+
+
+//LESSON 3 - Accessing Elements
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0];
+console.log(listItem);
+
+console.log(famousSayings[2]);
+
+console.log(famousSayings[3]); //This returns undefined
+
+/*
+1. Individual elements in arrays can also be stored to variables.
+
+Create a variable named listItem and set it equal to the first item in the famousSayings array using square bracket notation ([]).
+
+Then use console.log() to print the listItem variable to the console.
+2. Now, console.log() the third element in the famousSayings array using bracket notation to access the element.
+
+Do not save the element to a new variable before you log it.
+3. Awesome, you can access each element in an array using the index. But what happens if you try to access an index that is beyond the last element?
+
+Try to log the item at index [3] of famousSayings to the console. What is logged to the console?
+*/
+
+
+//LESSON 4 - Update Elements
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = 'avocados';
+
+/*
+1. Change the second element of the array groceryList to 'avocados'.
+*/
+
+
 //LESSON 5 - Arrays with let and const
 let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
 
