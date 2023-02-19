f4d7814e20204856d6620f855eda5f06bc5bfb6a
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-19 06:30:07 -0300

    UPDATE

--- 01-INTRODUCTION/introToJavaScript.js
+++ 01-INTRODUCTION/introToJavaScript.js
@@ -70,3 +70,48 @@ Note: You should concatenate the two strings exactly (without introducing any ad
 */
 
 
+//LESSON 7 - Properties
+
+console.log("Teaching the world how to code".length);
+
+/*
+1.Use the .length property to log the number of characters in the following string to the console:
+
+'Teaching the world how to code'
+*/
+
+
+//LESSON 8 - Methods
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+/*
+1. Use the .toUpperCase() method to log the string 'Codecademy' to the console in all capital letters.
+2. In the second console.log() statement in app.js, we have a string ' Remove whitespace ' which has spaces before and after the words 'Remove whitespace'.
+
+If we browse the JavaScript string documentation, we find several built-in string methods that each accomplish a different goal. The one method that seems ideal for us is .trim().
+
+Use the method to remove the whitespace at the beginning and end of the string in the second console.log() statement.
+*/
+
+
+//LESSON 9 - Built-in Objects
+
+console.log(Number.isInteger(Math.random() * 2017));
+
+/*
+1. Inside of a console.log(), create a random number with Math.random(), then multiply it by 100.
+2. Now, use Math.floor() to make the output a whole number.
+
+Inside the console.log() you wrote in the last step, put the existing Math.random() * 100 code inside the parentheses of Math.floor().
+3. Find a method on the JavaScript Math object that returns the smallest integer greater than or equal to a decimal number.
+
+Use this method with the number 43.8. Log the answer to the console.
+4. Use the JavaScript documentation to find a method on the built-in Number object that checks if a number is an integer.
+
+Put the number 2017 in the parentheses of the method and use console.log() to print the result.
+*/
