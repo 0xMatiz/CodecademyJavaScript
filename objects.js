4033484bf1bb4df6518bd4e58ef6c323bea0b3e2
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-19 02:44:47 -0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -187,3 +187,45 @@ console.log(spaceship);
 */
 
 
+//LESSON 9 - Looping Through Objects
+
+let spaceship = {
+  crew: {
+  captain: { 
+      name: 'Lily', 
+      degree: 'Computer Engineering', 
+      cheerTeam() { console.log('You got this!') } 
+      },
+  'chief officer': { 
+      name: 'Dan', 
+      degree: 'Aerospace Engineering', 
+      agree() { console.log('I agree, captain!') } 
+      },
+  medic: { 
+      name: 'Clementine', 
+      degree: 'Physics', 
+      announce() { console.log(`Jets on!`) } },
+  translator: {
+      name: 'Shauna', 
+      degree: 'Conservation Science', 
+      powerFuel() { console.log('The tank is full!') } 
+      }
+  }
+}; 
+
+// Write your code below
+
+for (let crewMember in spaceship.crew) { //below it takes the first index
+console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+};
+
+for (let crewMember in spaceship.crew) { //below we put 2 different index
+console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`);
+};
+
+
+/* 
+1. Using for...in, iterate through the spaceship.crew object in the code editor and console.log() a list of crew roles and names in the following format: '[crew member's role]: [crew member's name]', e.g.,'chief officer: Dan'.
+2. Using for...in, iterate through the spaceship.crew object in the code editor and console.log() a list of crew names and degrees in the following format: '[crew member's name]: [crew member's degree]', i.e.,'Lily: Computer Engineering'.
+3. Using for...in, iterate through the spaceship.crew object in the code editor and console.log() a list of crew names and degrees in the following format: '[crew member's name]: [crew member's degree]', i.e.,'Lily: Computer Engineering'. 
+*/
